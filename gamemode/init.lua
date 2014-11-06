e2add4c0d20389cc0eaa74f6c0d69c66cceb7466
commandercool <erohin_a_v@mail.ru>
2014-11-06 16:01:22 +0300

    Spawn clothes entity on player spawn

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,14 +5,21 @@ include( 'shared.lua' )
 
 function GM:PlayerSpawn( ply )
     self.BaseClass:PlayerSpawn( ply )   
- 
     ply:SetGravity  ( 1 )  
     ply:SetMaxHealth( 100, true )  
- 
     ply:SetWalkSpeed( 190 )  
     ply:SetRunSpeed ( 235 ) 
- 
 	ply:SetModel("models/player/dod_german.mdl")
+	
+	local item = ents.Create( "clothes" )
+	item:SetNWString( "name", "clothes" )
+	item:SetNWString( "itemName", name )
+	item:SetNWInt( "uID", 1 )
+	item:SetNWBool( "pickup", true )
+	item:SetNWEntity( "owner", ply )
+	item:SetItemName("clothes1")
+	item:Spawn()
+	item:Activate()
 end
 
 function GM:PlayerLoadout( ply )
