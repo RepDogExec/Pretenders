cf215f00e33a97011dbbddd3e640f1ba6c4642c8
commandercool <erohin_a_v@mail.ru>
2014-11-07 02:04:27 +0300

    Entity tooltip when player is near by

--- entities/entities/clothes/cl_init.lua
+++ entities/entities/clothes/cl_init.lua
@@ -1,8 +1,14 @@
 include("shared.lua")
 
+local drawTip = false
+
 function ENT:Think()
 end
 
 function ENT:Draw()
-	self:DrawModel()
+	self:AddEffects( 256 )
+    self:DrawModel()
+	if LocalPlayer():GetPos():Distance(self:GetPos()) < 200 then
+		AddWorldTip( self:EntIndex(), "COP'S CLOTHES", 0.5, self:GetPos(), self  )
+	end
 end
