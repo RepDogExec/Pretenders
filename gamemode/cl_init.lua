2a821b368916c8082f464119d3429f4faf508e40
commandercool <erohin_a_v@mail.ru>
2014-11-06 16:00:42 +0300

    Thirdperson view

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,15 @@
 include( 'shared.lua' )
+
+function ThirdPerson( ply, pos, ang, fov )
+	local view = {};
+	view.origin = pos - ( ang:Forward() * 100 );
+	view.angles = ang;
+	view.angles.p = ang.p - 10;
+	view.fov = fov;
+			
+	return view;
+end;
+hook.Add( "CalcView", "ThirdPerson", ThirdPerson );
+hook.Add( "ShouldDrawLocalPlayer", "MyShouldDrawLocalPlayer", function( ply )
+	 return true
+end )
